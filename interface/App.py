1a9d60785b15151ec7a4ce90dcaa547f6126f6ba
Asad <muhammadasadk42@gmail.com>
2025-10-08 22:28:14 +0500

    Added tidy and SRP following main.py along with App.py for basic testing , as well as added __init__.py to treat models directory as package for less bulky imports

---
+++ interface/App.py
@@ -0,0 +1,10 @@
+from models import *
+
+class App:
+    def __init__(self) -> None:
+        pass
+
+    @staticmethod
+    def run():
+      a = Student("Asad", "87", 2) 
+      print(a)    
---
+++ models/__init__.py
@@ -0,0 +1,15 @@
+from .person import Person
+from .teacher import Teacher
+from .student import Student
+from .student_list import Student_List
+from .course import Course
+from .course_list import Course_List
+
+__all__ = [
+    'Person',
+    'Teacher',
+    'Student',
+    'Student_List',
+    'Course',
+    'Course_List'
+]
